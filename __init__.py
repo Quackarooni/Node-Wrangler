90d4512f009a73dfb20a088ecf69bf1938fd9a3a
Quackers <shadowrogue624@gmail.com>
2023-12-10 14:18:21 +0800

    Update version numbers to 3.47 and Blender 4.0.0

--- __init__.py
+++ __init__.py
@@ -6,8 +6,8 @@
 bl_info = {
     "name": "Forked Wrangler",
     "author": "Bartek Skorupa, Greg Zaal, Sebastian Koenig, Christian Brinkmann, Florian Meyer",
-    "version": (3, 45),
-    "blender": (3, 5, 0),
+    "version": (3, 47),
+    "blender": (4, 0, 0),
     "location": "Node Editor Toolbar or Shift-W",
     "description": "Various tools to enhance and speed up node-based workflow",
     "warning": "",
